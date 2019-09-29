1fd1b76303c5bc95f200651aae0e69165fbdb3c0
Robin <imrobin.xi@gmail.com>
2019-09-29 16:52:28 +0800

    修改crop-monitor初始化

--- crop-monitor.js
+++ crop-monitor.js
@@ -10,8 +10,10 @@ const chokidar = require('chokidar');
 const log = console.log.bind(console);
 const IGNORE_FILES = /(^\..+)|(.+[\/\\]\..+)/; // 忽略文件.开头
 const SCALE_SIZE_ARRAY = [ 80, 110, 240, 400, 800, 960 ];
+const ONLY_WATCH_NEW_FILE = true;//是否只检测新增文件
 
 let imgDir = 'images'; // 默认图片文件夹
+let isInited = false;
 
 // 监控文件夹
 var watcher = chokidar.watch(imgDir, {
@@ -23,28 +25,33 @@ var watcher = chokidar.watch(imgDir, {
 });
 
 // 监听增加，删除文件的事件
-watcher.on('all', (event, imgPath) => {
-    let option = {};
-    switch (event) {
-        case 'add':
+watcher
+    .on('add', function (imgPath) {
+        if (isInited) {
             option = {
                 event: 'add',
                 imgPath: imgPath,
                 widthList: SCALE_SIZE_ARRAY
             };
             cropSub.send(option);
-            break;
-        case 'unlink':
+        }
+    })
+    .on('unlink', function (imgPath) {
+        if (isInited) {
             option = {
                 event: 'unlink',
                 imgPath: imgPath,
                 widthList: SCALE_SIZE_ARRAY
             };
             cropSub.send(option);
-            break;
-        default:
-            break;
-    }
-});
+        }
+    })
+    .on('error', function (error) {
+        console.info('发生了错误：', error);
+    })
+    .on('ready', function () {
+        isInited = true;
+        log('初始化扫描文件完成,准备监听');
+    });
 
 log('图片裁切监控已经启动');
