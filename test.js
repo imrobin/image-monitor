5404c4ca0abfeacee5b81584527abe2edb89c43d
Robin <imrobin.xi@gmail.com>
2019-11-07 22:53:13 +0800

    增加裁切时，增加图片高宽比校验

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ let watcherOpts = {
 
 let cropOpts = {
     savePath: 'images',
+    maxCropScale: 1.5,
     quality: 60,
     scaleSizeList: [{ width: 80, height: 80 }, { width: 110, height: 110 }, { width: 240, height: 240 }, { width: 400, height: 400 }, { width: 800, height: 800 }, { width: 960, height: 960 }]
 };
